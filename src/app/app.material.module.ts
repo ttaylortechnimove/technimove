5f353f261797cd9891a6036ae7a01f8ca4c536b7
Tony Taylor <ttaylortechnimove@gmail.com>
2017-12-27 15:23:09 +0000

    first commit

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -2,27 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-//import { MatOptionModule } from '@angular/material/option';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     imports: [
         MatButtonModule,
+        MatCardModule,
         MatNativeDateModule,
         MatCheckboxModule,
         MatFormFieldModule,
+        MatIconModule,
         MatInputModule,
         MatToolbarModule
     ],
     exports: [ 
         MatButtonModule,
+        MatCardModule,
         MatNativeDateModule,
         MatCheckboxModule,
         MatFormFieldModule,
+        MatIconModule,
         MatInputModule,
         MatToolbarModule 
     ]
